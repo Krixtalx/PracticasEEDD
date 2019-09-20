fb4e7e3ae35b93991ceeb846adc1ea8f8d6bf334
JC <JC@10.82.190.198>
2019-09-20 17:33:58 +0200

    Fixes, metodos en cascada y prueba en main

--- P1/main.cpp
+++ P1/main.cpp
@@ -18,23 +18,67 @@
 
 using namespace std;
 
-/*
- * 
- */
+void mostrarVDinInt(VDinamico<int> &vD){
+    cout << "tamF: " << vD.getTamF() << " tamL:" << vD.getTamL() << endl;
+    for (int i = 0; i < vD.getTamL(); i++) {
+        cout << vD[i] << endl;
+    }
+}
+
+
 int main(int argc, char** argv) {
 
     VDinamico<int> a(5);
-    int xd;
+
+    srand(time(NULL));
     for (int i = 0; i < 5; i++) {
-        xd = rand() % 100;
-        a[i] = xd;
+        a[i] = rand() % 100;
     }
-    cout << a[0] << "-" << a[1] << "-" << a[2] << "-" << a[3] << "-" << a[4] << endl;
-    a.eliminarDato(2);
-    cout << a[0] << "-" << a[1] << "-" << a[2] << "-" << a[3] << "-" << a[4] << endl;
-
-
-
+    mostrarVDinInt(a);
+    
+    int illoPosUnIntRandomQueMeHeInventaoJaJaEkisde = 1;
+    a.insertarDato(illoPosUnIntRandomQueMeHeInventaoJaJaEkisde, 3);
+    mostrarVDinInt(a);
+    
+    a.eliminarDato(3);
+    mostrarVDinInt(a);    
+    
+//    cout << "    OOOOOOOOOO    " << endl <<
+//            "  OOOOOOOOOOOOOO  " << endl << 
+//            "OOOO          OOOO" << endl <<
+//            "OOOO          OOOO" << endl <<
+//            "OOOO          OOOO" << endl <<
+//            "OOOO          OOOO" << endl <<
+//            "OOOO          OOOO" << endl <<
+//            "OOOO          OOOO" << endl <<
+//            "OOOO          OOOO" << endl <<
+//            "OOOO          OOOO" << endl <<
+//            "  OOOOOOOOOOOOOO  " << endl <<
+//            "    OOOOOOOOOO    " << endl << endl <<
+//            "WW      WW      WW" << endl <<
+//            " WW    WWWW    WW " << endl <<
+//            "  WW  WW  WW  WW  " << endl <<
+//            "   WWWW    WWWW   " << endl <<
+//            "    WW      WW    " << endl << endl <<
+//            "    OOOOOOOOOO    " << endl <<
+//            "  OOOOOOOOOOOOOO  " << endl << 
+//            "OOOO          OOOO" << endl <<
+//            "OOOO          OOOO" << endl <<
+//            "OOOO          OOOO" << endl <<
+//            "OOOO          OOOO" << endl <<
+//            "OOOO          OOOO" << endl <<
+//            "OOOO          OOOO" << endl <<
+//            "OOOO          OOOO" << endl <<
+//            "OOOO          OOOO" << endl <<
+//            "  OOOOOOOOOOOOOO  " << endl <<
+//            "    OOOOOOOOOO    " << endl;
+    
+    int o = 11;
+    a.insertarDato(o).insertarDato(o).insertarDato(o).insertarDato(o);
+    mostrarVDinInt(a);
+    a.eliminarDato(0).eliminarDato(0).eliminarDato(0).eliminarDato(0).eliminarDato(0);
+    mostrarVDinInt(a);
+    
     return 0;
 }
 
