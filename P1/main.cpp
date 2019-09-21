5c3b4baaf0c4fe754670d8ae0339aa16c2f9cb39
José Antonio <jocoara1213@gmail.com>
2019-09-21 21:13:56 +0200

    Añadido el Main del ejercicio

--- P1/main.cpp
+++ P1/main.cpp
@@ -1,76 +1,110 @@
 #include <cstdlib>
+#include <vector>
+#include <list>
+#include <string>
+#include <fstream>
+#include <sstream>
 #include <iostream>
-#include <stdlib.h>
-#include "VDinamico.h"
+
 
 using namespace std;
 
-void mostrarVDinInt(VDinamico<int> &vD){
-    cout << "tamF: " << vD.getTamF() << " tamL:" << vD.getTamL() << endl;
-    for (int i = 0; i < vD.getTamL(); i++) {
-        cout << vD[i] << " - ";
-    }
-    cout <<endl;
+/*
+ coordenadas UTM formadas por latitud y longitud 
+*/
+
+    struct UTM{
+      double latitud;
+      double longitud;
+      UTM (double _lat, double _long): latitud(_lat), longitud (_long){}
+    };
+    
+    
+ /**
+ Clase Cliente
+ **/   
+
+class Cliente {
+    string dni;
+    string pass;
+    string nombre;
+    string direccion;
+    UTM posicion;
+    
+public:
+    //Constructor
+    Cliente(string _dni, string _pass, string _nombre, string _direccion, double _latitud, double _longitud):
+    dni(_dni), pass(_pass), nombre(_nombre), direccion (_direccion), posicion (_latitud, _longitud){}
+};
+
+
+void leeClientes(string fileNameClientes){
+    ifstream fe;                    //Flujo de entrada
+    string linea;                   //Cada línea tiene un clienete
+    int total = 0;                  //Contador de líneas o clientes
+    
+    //Variables auxiliares para almacenar los valores leídos
+    string dni, nombre, pass, direccion, latitud, longitud;
+    double dlat, dlon; 
+
+
+    
+    //Asociamos el flujo al fichero 
+    fe.open(fileNameClientes);
+    
+    if(fe.good()){
+        //Mientras no se haya llegado al final del fichero
+        while(!fe.eof()){
+            getline(fe, linea);     //Toma una línea del fichero
+            stringstream ss;        //Stream que trabaja sobre buffer interno         
+
+            if(linea!=""){
+                ++total;
+            }
+
+            if(total>1){                
+                //Inicializamos el contenido de ss
+                ss << linea;
+                
+                //Leemos el NIF
+                getline(ss,dni,';');            //El carácter ; se lee y se elimina de ss
+                
+                //Leemos el pass
+                getline(ss,pass,';');           //El caráter ; se lee y se elimina de ss
+
+                //Leemos el nombre
+                getline(ss,nombre,';');         //El caráter ; se lee y se elimina de ss
+                
+                //Leemos la dirección
+                getline(ss,direccion,';');      //El caráter ; se lee y se elimina de ss
+                
+                //Leemos la latitud y longitud
+                getline(ss,latitud,';');        //El caráter ; se lee y se elimina de ss
+                getline(ss,longitud,';');       //El caráter ; se lee y se elimina de ss
+
+                dlat = stod(latitud);
+                dlon = stod(longitud);
+                
+                //con todos los atributos leídos, se crea el cliente
+                Cliente client (dni, pass, nombre, direccion,dlat, dlon);
+                cout << "leido cliente " << total << "  ";
+            }        
+        }
+            
+        cout<<"Total de clientes en el fichero: " << total <<endl;
+        fe.close(); //Cerramos el flujo de entrada
+    }else{
+        cerr<<"No se puede abrir el fichero"<<endl;
+    }    
 }
 
+	
 
 int main(int argc, char** argv) {
-    try{
-        VDinamico<int> test(5);
 
-        srand(time(NULL));
-        for (int i = 0; i < 5; i++) {
-            test[i] = (rand() % 100);
-        }
-        mostrarVDinInt(test);
-
-        int illoPosUnIntRandomQueMeHeInventaoJaJaEkisde = 1;
-        test.insertarDato(illoPosUnIntRandomQueMeHeInventaoJaJaEkisde, 3);
-        test.insertarDato(34, 4);
-        mostrarVDinInt(test);
-
-        test.eliminarDato(3);
-        mostrarVDinInt(test);    
-
-        int o = 11;
-        test.insertarDato(o).insertarDato(o);
-        mostrarVDinInt(test);
-        test.eliminarDato(0);
-        mostrarVDinInt(test);
-    }catch(std::out_of_range e){
-        cerr<<e.what();
-    }
+    cout << "Comienzo de lectura de un fichero " << endl;
+    
+    leeClientes ("clientes_v2.csv");
     return 0;
 }
 
-    
-//    cout << "    OOOOOOOOOO    " << endl <<
-//            "  OOOOOOOOOOOOOO  " << endl << 
-//            "OOOO          OOOO" << endl <<
-//            "OOOO          OOOO" << endl <<
-//            "OOOO          OOOO" << endl <<
-//            "OOOO          OOOO" << endl <<
-//            "OOOO          OOOO" << endl <<
-//            "OOOO          OOOO" << endl <<
-//            "OOOO          OOOO" << endl <<
-//            "OOOO          OOOO" << endl <<
-//            "  OOOOOOOOOOOOOO  " << endl <<
-//            "    OOOOOOOOOO    " << endl << endl <<
-//            "WW      WW      WW" << endl <<
-//            " WW    WWWW    WW " << endl <<
-//            "  WW  WW  WW  WW  " << endl <<
-//            "   WWWW    WWWW   " << endl <<
-//            "    WW      WW    " << endl << endl <<
-//            "    OOOOOOOOOO    " << endl <<
-//            "  OOOOOOOOOOOOOO  " << endl << 
-//            "OOOO          OOOO" << endl <<
-//            "OOOO          OOOO" << endl <<
-//            "OOOO          OOOO" << endl <<
-//            "OOOO          OOOO" << endl <<
-//            "OOOO          OOOO" << endl <<
-//            "OOOO          OOOO" << endl <<
-//            "OOOO          OOOO" << endl <<
-//            "OOOO          OOOO" << endl <<
-//            "  OOOOOOOOOOOOOO  " << endl <<
-//            "    OOOOOOOOOO    " << endl;
-    
