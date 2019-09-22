26a1e9924d32aeaaf3789a2be9ba98073be78522
JC <jcfer@DESKTOP-VCNCFKO>
2019-09-22 11:37:07 +0200

    Implementacion de Ordenar y Buscar

--- P1/main.cpp
+++ P1/main.cpp
@@ -5,24 +5,28 @@
 #include <fstream>
 #include <sstream>
 #include <iostream>
+#include <exception>
+
+#include "VDinamico.h"
 
 
 using namespace std;
 
 /*
  coordenadas UTM formadas por latitud y longitud 
-*/
+ */
 
-    struct UTM{
-      double latitud;
-      double longitud;
-      UTM (double _lat, double _long): latitud(_lat), longitud (_long){}
-    };
-    
-    
- /**
- Clase Cliente
- **/   
+struct UTM {
+    double latitud;
+    double longitud;
+
+    UTM(double _lat, double _long) : latitud(_lat), longitud(_long) {
+    }
+};
+
+/**
+Clase Cliente
+ **/
 
 class Cliente {
     string dni;
@@ -30,81 +34,104 @@ class Cliente {
     string nombre;
     string direccion;
     UTM posicion;
-    
+
 public:
     //Constructor
-    Cliente(string _dni, string _pass, string _nombre, string _direccion, double _latitud, double _longitud):
-    dni(_dni), pass(_pass), nombre(_nombre), direccion (_direccion), posicion (_latitud, _longitud){}
+
+    Cliente(string _dni, string _pass, string _nombre, string _direccion, double _latitud, double _longitud) :
+    dni(_dni), pass(_pass), nombre(_nombre), direccion(_direccion), posicion(_latitud, _longitud) {
+    }
 };
 
+void leeClientes(string fileNameClientes) {
+    ifstream fe; //Flujo de entrada
+    string linea; //Cada línea tiene un clienete
+    int total = 0; //Contador de líneas o clientes
 
-void leeClientes(string fileNameClientes){
-    ifstream fe;                    //Flujo de entrada
-    string linea;                   //Cada línea tiene un clienete
-    int total = 0;                  //Contador de líneas o clientes
-    
     //Variables auxiliares para almacenar los valores leídos
     string dni, nombre, pass, direccion, latitud, longitud;
-    double dlat, dlon; 
+    double dlat, dlon;
 
 
-    
+
     //Asociamos el flujo al fichero 
     fe.open(fileNameClientes);
-    
-    if(fe.good()){
-        //Mientras no se haya llegado al final del fichero
-        while(!fe.eof()){
-            getline(fe, linea);     //Toma una línea del fichero
-            stringstream ss;        //Stream que trabaja sobre buffer interno         
 
-            if(linea!=""){
+    if (fe.good()) {
+        //Mientras no se haya llegado al final del fichero
+        while (!fe.eof()) {
+            getline(fe, linea); //Toma una línea del fichero
+            stringstream ss; //Stream que trabaja sobre buffer interno         
+
+            if (linea != "") {
                 ++total;
             }
 
-            if(total>1){                
+            if (total > 1) {
                 //Inicializamos el contenido de ss
                 ss << linea;
-                
+
                 //Leemos el NIF
-                getline(ss,dni,';');            //El carácter ; se lee y se elimina de ss
-                
+                getline(ss, dni, ';'); //El carácter ; se lee y se elimina de ss
+
                 //Leemos el pass
-                getline(ss,pass,';');           //El caráter ; se lee y se elimina de ss
+                getline(ss, pass, ';'); //El caráter ; se lee y se elimina de ss
 
                 //Leemos el nombre
-                getline(ss,nombre,';');         //El caráter ; se lee y se elimina de ss
-                
+                getline(ss, nombre, ';'); //El caráter ; se lee y se elimina de ss
+
                 //Leemos la dirección
-                getline(ss,direccion,';');      //El caráter ; se lee y se elimina de ss
-                
+                getline(ss, direccion, ';'); //El caráter ; se lee y se elimina de ss
+
                 //Leemos la latitud y longitud
-                getline(ss,latitud,';');        //El caráter ; se lee y se elimina de ss
-                getline(ss,longitud,';');       //El caráter ; se lee y se elimina de ss
+                getline(ss, latitud, ';'); //El caráter ; se lee y se elimina de ss
+                getline(ss, longitud, ';'); //El caráter ; se lee y se elimina de ss
 
                 dlat = stod(latitud);
                 dlon = stod(longitud);
-                
-                //con todos los atributos leídos, se crea el cliente
-                Cliente client (dni, pass, nombre, direccion,dlat, dlon);
-                cout << "leido cliente " << total << "  ";
-            }        
-        }
-            
-        cout<<"Total de clientes en el fichero: " << total <<endl;
-        fe.close(); //Cerramos el flujo de entrada
-    }else{
-        cerr<<"No se puede abrir el fichero"<<endl;
-    }    
-}
 
-	
+                //con todos los atributos leídos, se crea el cliente
+                Cliente client(dni, pass, nombre, direccion, dlat, dlon);
+                cout << "leido cliente " << total << "  ";
+            }
+        }
+
+        cout << "Total de clientes en el fichero: " << total << endl;
+        fe.close(); //Cerramos el flujo de entrada
+    } else {
+        cerr << "No se puede abrir el fichero" << endl;
+    }
+}
 
 int main(int argc, char** argv) {
 
     cout << "Comienzo de lectura de un fichero " << endl;
-    
-    leeClientes ("clientes_v2.csv");
+
+    //    leeClientes ("clientes_v2.csv");
+
+    srand(1);
+    VDinamico<int> test(5);
+    for (int i = 0; i < 5; i++) {
+        test[i] = rand() % 100;
+        cout << test[i] << " ";
+    }
+    cout << endl;
+
+    test.ordenar();
+    for (int i = 0; i < 5; i++) {
+        cout << test[i] << " ";
+    }
+    cout << endl;
+
+    int wanted = 12;
+    try {
+        int found = test.busca(wanted);
+        cout << endl << "ENCONTRADO: " << found+1 << "(" << found << ")" << endl;
+    } catch (std::out_of_range& e) {
+        cout << e.what();
+    }
+
+
     return 0;
 }
 
