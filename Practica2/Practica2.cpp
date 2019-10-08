8dddc9c7072b604f1b5e56253d4915d9d1c26060
jcfp0003 <jcfp0003@red.ujaen.es>
2019-10-08 20:22:04 +0200

    Test include

--- Practica2/Practica2.cpp
+++ Practica2/Practica2.cpp
@@ -2,11 +2,25 @@
 //
 
 #include <iostream>
+#include <ctime>
+#include "VDinamico.h"
 
+
+using namespace std;
 int main()
 {
-    std::cout << "Hello World!\n";
-	std::cout << "OwO";
+	VDinamico<int> ola;
+	srand(time(0));
+	for (int i = 10; i >= 0; i--)
+	{
+		int owo = rand() % 100;
+		ola.insertarDato(owo);
+	}
+
+	for (int j = 0; j < ola.getTamL(); j++)
+	{
+		cout << ola[j] << endl;
+	}
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
