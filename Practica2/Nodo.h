9bd8a2b1b2785b8a5d54d521067a4e5231cb3632
jcfp0003 <jcfp0003@red.ujaen.es>
2019-10-10 10:59:27 +0200

    Constructores Nodo y F P2

--- Practica2/Nodo.h
+++ Practica2/Nodo.h
@@ -6,53 +6,62 @@
 template <class T>
 class Nodo {
 private:
-    Nodo<T>* anterior;
-    Nodo<T>* siguiente;
-    T dato;
+	Nodo<T>* anterior;
+	Nodo<T>* siguiente;
+	T dato;
 public:
-    Nodo();
-    Nodo(const Nodo& orig);
-    virtual ~Nodo();
-    
-    Nodo<T>* GetAnterior() const;
-    Nodo<T>* SetAnterior(Nodo<T> anterior);
-    Nodo<T>* GetSiguiente() const;
-    Nodo<T>* SetSiguiente(Nodo<T> siguiente);
-    T GetDato() const;
-    Nodo<T>* SetDato(T dato);
-    
+	Nodo();
+	Nodo(const Nodo& orig);
+	virtual ~Nodo();
+
+	Nodo<T>* GetAnterior() const;
+	Nodo<T>* SetAnterior(Nodo<T>* anterior);
+	Nodo<T>* GetSiguiente() const;
+	Nodo<T>* SetSiguiente(Nodo<T>* siguiente);
+	T GetDato() const;
+	Nodo<T>* SetDato(T dato);
+
 };
-    template <class T>
-    Nodo<T>* Nodo<T>::GetAnterior() const {
-        return anterior;
-    }
-    
-    template <class T>
-    Nodo<T>* Nodo<T>::SetAnterior(Nodo<T>* anterior) {
-        this->anterior = anterior;
-        return this;
-    }
+template<class T>
+Nodo<T>::Nodo() {}
 
-    template <class T>
-    Nodo<T>* Nodo<T>::GetSiguiente() const {
-        return siguiente;
-    }
+template<class T>
+Nodo<T>::Nodo(const Nodo& orig) {}
 
-    template <class T>
-    Nodo<T>* Nodo<T>::SetSiguiente(Nodo* siguiente) {
-        this->siguiente = siguiente;
-        return this;
-    }
-    
-    template <class T>
-    T Nodo<T>::GetDato() const {
-        return dato;
-    }
+template<class T>
+Nodo<T>::~Nodo() {}
 
-    template <class T>
-    Nodo<T>* Nodo<T>::SetDato(T dato) {
-        this->dato = dato;
-        return this;
-    }
+template <class T>
+Nodo<T>* Nodo<T>::GetAnterior() const {
+	return anterior;
+}
+
+template <class T>
+Nodo<T>* Nodo<T>::SetAnterior(Nodo<T>* anterior) {
+	this->anterior = anterior;
+	return this;
+}
+
+template <class T>
+Nodo<T>* Nodo<T>::GetSiguiente() const {
+	return siguiente;
+}
+
+template <class T>
+Nodo<T>* Nodo<T>::SetSiguiente(Nodo* siguiente) {
+	this->siguiente = siguiente;
+	return this;
+}
+
+template <class T>
+T Nodo<T>::GetDato() const {
+	return dato;
+}
+
+template <class T>
+Nodo<T>* Nodo<T>::SetDato(T dato) {
+	this->dato = dato;
+	return this;
+}
 
 #endif /* NODO_H */
