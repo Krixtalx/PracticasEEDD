93a48301362231128802eb373aca171c90fb1b19
José Antonio Collado Araque <Niskprogramming@outlook.com>
2019-10-11 19:26:29 +0200

    Documentación añadida

--- Practica2/Nodo.h
+++ Practica2/Nodo.h
@@ -23,13 +23,22 @@ public:
 	Nodo<T>* SetDato(T dato);
 };
 
-
+/**
+*@Brief Constructor por defecto
+*/
 template<class T>
 Nodo<T>::Nodo() : anterior(0), siguiente(0) {}
 
+/**
+*@Brief Constructor parametrizado
+*@param dato con el que crear el nodo
+*/
 template<class T>
 Nodo<T>::Nodo(T& _dato) : dato(_dato), anterior(0), siguiente(0) {}
 
+/**
+*@Brief Constructor de copia
+*/
 template<class T>
 Nodo<T>::Nodo(const Nodo& orig) {
 	this->anterior = 0;
@@ -37,36 +46,60 @@ Nodo<T>::Nodo(const Nodo& orig) {
 	this->dato = orig.dato;
 }
 
+/**
+*@Brief Destructor
+*/
 template<class T>
 Nodo<T>::~Nodo() {}
 
+/**
+*@Brief Getter del nodo anterior
+*/
 template <class T>
 Nodo<T>* Nodo<T>::GetAnterior() const {
 	return anterior;
 }
 
+/**
+*@Brief Setter del nodo anterior
+*@param Nodo que se desea poner como anterior
+*/
 template <class T>
 Nodo<T>* Nodo<T>::SetAnterior(Nodo<T>* anterior) {
 	this->anterior = anterior;
 	return this;
 }
 
+/**
+*@Brief Getter del nodo siguiente
+*/
 template <class T>
 Nodo<T>* Nodo<T>::GetSiguiente() const {
 	return siguiente;
 }
 
+/**
+*@Brief Setter del nodo anterior
+*@param Nodo que se desea poner como anterior
+*/
 template <class T>
 Nodo<T>* Nodo<T>::SetSiguiente(Nodo* siguiente) {
 	this->siguiente = siguiente;
 	return this;
 }
 
+/**
+*@Brief Getter del dato
+*/
 template <class T>
 T& Nodo<T>::GetDato() const {
 	return dato;
 }
 
+/**
+*@Brief Setter del dato
+*@param dato que se desea poner
+*/
 template <class T>
 Nodo<T>* Nodo<T>::SetDato(T dato) {
 	this->dato = dato;
