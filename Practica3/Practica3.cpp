df7c0f9f8dcc2b6404d93677ecb748938c3fb144
José Antonio Collado Araque <Niskprogramming@outlook.com>
2019-10-30 11:44:28 +0100

    Limpieza del main

--- Practica3/Practica3.cpp
+++ Practica3/Practica3.cpp
@@ -8,18 +8,7 @@
 int main(){
 
 	setlocale(LC_ALL, "spanish");
-	//EcoCityMoto test;
-	//if (menuPrincipal(test))
-	//	return 0;
 
-	//AVL<int> arbol;
-	//for (int i = 0; i <= 40; i++)
-	//{
-	//	arbol.inserta(i);
-	//	arbol.verArbol();
-	//	std::cout << "Altura " << i << " : " << arbol.altura() << std::endl;
-	//}
-	//std::cout<<arbol.altura();
 	EcoCityMoto appWapa;
 	Cliente test("7");
 	test.setAplicacion(&appWapa);
