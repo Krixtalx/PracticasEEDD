2dd7f9c8243a90a5b3d60e499993721d174ced59
José Antonio Collado Araque <Niskprogramming@outlook.com>
2019-11-15 13:21:25 +0100

    Funciones de la tabla Hash declaradas

--- Practica5/THashCliente.cpp
+++ Practica5/THashCliente.cpp
@@ -1,19 +1,66 @@
 #include "THashCliente.h"
 
-unsigned long djb2(std::string& palabra){
-	unsigned long hash = 5381;
-		int c;
-
-        for(int i=0 ; i<palabra.length() ; i++){
-			c=(unsigned long)palabra[i];
-            hash = ((hash << 5) + hash) + c; 
-			}
-
-        return hash;
+int THashCliente::hash(unsigned long clave, int intento)
+{
+	return 0;
 }
 
-bool THashCliente::insertar(unsigned long clave, string& dni, Cliente& cliente)
-{
+THashCliente::THashCliente(int tamTabla): tamatabla(tamTabla){
+
+}
+
+unsigned long THashCliente::djb2(string& palabra){
+	unsigned long hash = 5381;
+	int c;
+
+	for (int i = 0; i < palabra.length(); i++) {
+		c = (unsigned long)palabra[i];
+		hash = ((hash << 5) + hash) + c;
+	}
+
+	return hash;
+}
+
+bool THashCliente::insertar(unsigned long clave, string& dni, Cliente& cliente){
 
 	return false;
+}
+
+bool THashCliente::buscar(unsigned long clave, string& dni, Cliente& cliente)
+{
+	return false;
+}
+
+bool THashCliente::borrar(unsigned long clave, string& dni)
+{
+	return false;
+}
+
+unsigned int THashCliente::numCliente()
+{
+	return 0;
+}
+
+void THashCliente::redispersar(unsigned tama)
+{
+}
+
+unsigned int THashCliente::maxColisiones()
+{
+	return 0;
+}
+
+unsigned int THashCliente::promedioColisiones()
+{
+	return 0;
+}
+
+float THashCliente::factorCarga()
+{
+	return 0.0f;
+}
+
+unsigned int THashCliente::tamaTabla()
+{
+	return 0;
 }
--- Practica5/THashCliente.h
+++ Practica5/THashCliente.h
@@ -11,15 +11,26 @@ using namespace std;
 class THashCliente
 {
 	vector<Cliente>* buffer = 0;
-	int numclientes = 0;
-	int tamatabla = 0;
+
+	unsigned int numclientes = 0;
+	unsigned int tamatabla = 0;
+	unsigned int maxColisiones = 0;
+	unsigned int numColisiones = 0;
+
 	int hash(unsigned long clave, int intento);
+
 public:
-	
+	THashCliente(int tamTabla);
 	unsigned long djb2(string& palabra);
 	bool insertar(unsigned long clave, string& dni, Cliente& cliente);
-	void suicidarse(t_time* hora);
-	
+	bool buscar(unsigned long clave, string& dni, Cliente& cliente);
+	bool borrar(unsigned long clave, string& dni);
+	unsigned int numCliente();
+	void redispersar(unsigned tama);
+	unsigned int maxColisiones();
+	unsigned int promedioColisiones();
+	float factorCarga();
+	unsigned int tamaTabla();
 
 };
 
