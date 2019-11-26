61365459833041754df2b7edee480276f60e42b3
jcfp0003 <jcfp0003@red.ujaen.es>
2019-11-26 16:37:37 +0100

    no se que esta pasando xdxd

--- Practica5/Practica5.cpp
+++ Practica5/Practica5.cpp
@@ -13,9 +13,13 @@ int main()
 	
 	setlocale(LC_ALL, "spanish");
 	//TODO: volver a crear itinerarios relacionando % de bateria y duracion
-	size_t tamtabla = 5000;
+	size_t tamtabla = 5413;
 	srand(time(0));
 	EcoCityMoto app(0, tamtabla);
-	login(app);
+	/*string archivo = "clientes.csv";
+	std::vector<Cliente*>* v = leerFich::ficheroaVector(archivo);
+	app.vectorToTabla(v, 5413, 103, 467);*/
+	cout << app.estadoTabla();
+	//login(app);
 	return 0;
 }
