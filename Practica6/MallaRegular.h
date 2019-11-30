5a2730d6b7af6888c7df7dc3286595b1a5336133
José Antonio <jocoara1213@gmail.com>
2019-11-30 22:04:17 +0100

    Hay un error, pero me voy a cenar F

--- Practica6/MallaRegular.h
+++ Practica6/MallaRegular.h
@@ -1,22 +1,40 @@
 #pragma once
 
 #include <vector>
+#include <list>
 #include "UTM.h"
 
 template <class T>
 class Celda {
-	list <T> buffer;
+
+	list<T> lista;
 public:
 	unsigned getNumEle();
 	T& masCercano(UTM coor);
-
 };
+
+
+template<class T>
+T& Celda<T>::masCercano(UTM coor)
+{
+	T* aux;
+	double distancia=9999;
+	if (lista.size() > 0) {
+		for (std::list<T>::iterator it=lista.begin(); it != lista.end(); it++){
+			if (coor.distancia(it->getUTM()) < distancia) {
+				distancia = coor.distancia(it->getUTM());
+				aux = it;
+			}
+		}
+	}
+	return *aux;
+}
 
 template <class T>
 class MallaRegular
 {
 	//TODO: atributos
-	vector<vector<Celda>> buffer;
+	vector<vector<Celda<T>>> buffer;
 	float xMin, xMax, yMin, yMax, interX, interY;
 public:
 	MallaRegular(float aXMin, float aYMin, float aXMax, float aYMax, int nDivX, int nDivY);
@@ -35,9 +53,9 @@ public:
 * 
 */
 template<class T>
-MallaRegular<T>::MallaRegular(float aXMin, float aYMin, float aXMax, float aYMax, int nDivX, int nDivY): xMin(aXMin), xMas(aXMax), yMin(aYMin), yMax(aYMax){
+MallaRegular<T>::MallaRegular(float aXMin, float aYMin, float aXMax, float aYMax, int nDivX, int nDivY): xMin(aXMin), xMax(aXMax), yMin(aYMin), yMax(aYMax){
 	buffer.resize(nDivX);
-	for (unsigned i = 0; i < length; i++){
+	for (unsigned i = 0; i < buffer.size(); i++){
 		buffer[i].resize(nDivY);
 	}
 	interX = (xMax - xMin) / nDivX;
@@ -47,11 +65,28 @@ MallaRegular<T>::MallaRegular(float aXMin, float aYMin, float aXMax, float aYMax
 template<class T>
 T& MallaRegular<T>::buscarCercano(float x, float y)
 {
-	UTM aux(x, y);
+	UTM coordenadas(x, y);
 	int posX = (x - xMin) / interX;
 	int posY = (yMax - y) / interY;
+	double distancia=9999;
+	T* aux, aux2;
 
-	return T();
+	if (posX!=0 && posX!=(xMax/interX)){
+		for (unsigned i = posX-1; i <= posX+1; i++)
+		{
+			if (posY != 0 && posY != (yMax / interY)) {
+				for (unsigned j = posY-1; j <= posY+1; j++)
+				{
+					aux2 = &(buffer[i][j].masCercano(coordenadas));
+					if (coordenadas.distancia(aux2->getUTM()) < distancia) {
+						distancia = coordenadas.distancia(aux2->getUTM());
+						aux = aux2;
+					}
+				}
+			}
+		}
+	}
+	return *aux;
 }
 
 template<class T>
@@ -60,5 +95,4 @@ MallaRegular<T>& MallaRegular<T>::insertar(T dato)
 
 	// TODO: insertar una instrucción return aquí
 }
-
 
